32e0db1e80d9ef106fd8f4ac7b0a1b243859ade4
konynour <nourmohamed913@outlook.com>
2026-02-17 04:01:49 +0200

    .

--- lib/models/movies_models.dart
+++ lib/models/movies_models.dart
@@ -31,39 +31,44 @@ class MoviesModels {
     required this.voteCount,
   });
 
-  MoviesModels.fromJson(Map<String, dynamic> json) {
-    adult = json['adult'];
-    backdropPath = json['backdrop_path'];
-    genreIds = json['genre_ids'].cast<int>();
-    id = json['id'];
-    originalLanguage = json['original_language'];
-    originalTitle = json['original_title'];
-    overview = json['overview'];
-    popularity = json['popularity'];
-    posterPath = json['poster_path'];
-    releaseDate = json['release_date'];
-    title = json['title'];
-    video = json['video'];
-    voteAverage = json['vote_average'];
-    voteCount = json['vote_count'];
+  factory MoviesModels.fromJson(Map<String, dynamic> json) {
+    return MoviesModels(
+    adult : json['adult'],
+    backdropPath : json['backdrop_path'],
+    genreIds : json['genre_ids'].cast<int>(),
+    id : json['id'],
+    originalLanguage : json['original_language'],
+    originalTitle : json['original_title'],
+    overview : json['overview'] ,
+    popularity : json['popularity'],
+    posterPath : json['poster_path'],
+    releaseDate : json['release_date'],
+    title : json['title'],
+    video : json['video'],
+    voteAverage : json['vote_average'],
+    voteCount : json['vote_count'],
+  );
   }
 
+
+
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['adult'] = adult;
-    data['backdrop_path'] = backdropPath;
-    data['genre_ids'] = genreIds;
-    data['id'] = id;
-    data['original_language'] = originalLanguage;
-    data['original_title'] = originalTitle;
-    data['overview'] = overview;
-    data['popularity'] = popularity;
-    data['poster_path'] = posterPath;
-    data['release_date'] = releaseDate;
-    data['title'] = title;
-    data['video'] = video;
-    data['vote_average'] = voteAverage;
-    data['vote_count'] = voteCount;
-    return data;
+   
+    return {
+      'adult': adult,
+      'backdrop_path': backdropPath,
+      'genre_ids': genreIds,
+      'id': id,
+      'original_language': originalLanguage,
+      'original_title': originalTitle,
+      'overview': overview,
+      'popularity': popularity,
+      'poster_path': posterPath,
+      'release_date': releaseDate,
+      'title': title,
+      'video': video,
+      'vote_average': voteAverage,
+      'vote_count': voteCount,
+    };
   }
 }
