a92318376a207b42cf1b049f893578ed2ccdd968
konynour <nourmohamed913@outlook.com>
2026-03-09 07:40:51 +0200

    provider

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,29 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:mvvm_statemanagements_project/constants/my_app_icons.dart';
-import 'package:mvvm_statemanagements_project/widgets/movies/movies_widget.dart';
+import 'package:mvvm_statemanagements_project/view_models/favorite_provider.dart';
+import 'package:provider/provider.dart';
+
+import '../constants/my_app_icons.dart';
+import '../widgets/movies/movies_widget.dart';
 
 class FavoritesScreen extends StatelessWidget {
   const FavoritesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final favoritesProvider = Provider.of<FavoritesProvider>(context);
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Favorites Movies"),
+        title: const Text("Favorite Movies"),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              favoritesProvider.clearAllFavs();
+            },
             icon: const Icon(MyAppIcons.delete, color: Colors.red),
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          // return Text("data")  ; 
-          return const MoviesWidget();
-        },
-      ),
+      body: favoritesProvider.favoritesList.isEmpty
+          ? const Center(
+              child: Text(
+                "No added Favs",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            )
+          : ListView.builder(
+              itemCount: favoritesProvider.favoritesList.length,
+              itemBuilder: (context, index) {
+                return ChangeNotifierProvider.value(
+                  value: favoritesProvider.favoritesList.reversed
+                      .toList()[index],
+                  child: const MoviesWidget(),
+                ); //const Text("data");
+              },
+            ),
     );
   }
 }
