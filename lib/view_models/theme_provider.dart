beba00e4eb77fd476e7c92d9c77a05c1f73e0a3f
konynour <nourmohamed913@outlook.com>
2026-03-12 09:19:12 +0200

    riverpod

---
+++ lib/view_models/theme_provider.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_riverpod/legacy.dart';
+import 'package:mvvm_statemanagements_project/enums/theme_enum.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeProvider extends StateNotifier<ThemeEnums> {
+  final prefsKey = "isDarkMode";
+  ThemeProvider() : super(ThemeEnums.light) {
+    _loadTheme();
+  }
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isDarkMode = prefs.getBool(prefsKey) ?? false;
+    state = isDarkMode ? ThemeEnums.dark : ThemeEnums.light;
+  }
+
+  Future<void> _toggleTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (state == ThemeEnums.dark) {
+      state = ThemeEnums.light;
+      await prefs.setBool(prefsKey, true);
+    } else {
+      state = ThemeEnums.dark;
+      await prefs.setBool(prefsKey, false);
+    }
+  }
+}
